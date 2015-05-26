d0da4dba87cccbaf6a66c6915d71c4645aeb0eaa
mebedw <wade.mebed@cbsinteractive.com>
2015-05-26 08:21:33 -0700

    Playing around with text boxes

--- sandbox/ViewController.swift
+++ sandbox/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tempText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func textFieldReturn(sender: AnyObject) {
+        sender.resignFirstResponder()
+    }
+
+
+    @IBAction func convertTemp(sender: AnyObject) {
+        let fahrenheit = (tempText.text as NSString).doubleValue
+        let celsius = (fahrenheit - 32)/1.8
+        let resultText = "Celsius \(celsius)"
+        resultLabel.text = resultText
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        tempText.endEditing(true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
